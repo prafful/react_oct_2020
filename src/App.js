0ade0979268b8be8aec4a077cdeb7f21c430ad5f
prafful <prraful@gmail.com>
2020-11-01 21:04:36 -0800

    states

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. It is easy!
         </a>
       </header>
     </div>
